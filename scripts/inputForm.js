5aa9f9144adb9647fd81688331ff0dc46f142c38
Jubayer-K <jubayerkhan21@gmail.com>
2024-02-19 01:17:39 +0600

    adding DOM features

---
+++ scripts/inputForm.js
@@ -0,0 +1,26 @@
+const phoneNumberInput = document.getElementById('phone-number');
+const nameInput = document.getElementById('passenger-name').value;
+
+phoneNumberInput.addEventListener("keyup", function (event) {
+    const text = event.target.value;
+    const nextButton = document.getElementById('btn-next');
+    if (text !== "") {
+        nextButton.removeAttribute("disabled");
+    } else {
+        nextButton.setAttribute("disabled", true);
+    }
+  });
+
+
+  const coupon = document.getElementById('coupon-input');
+  coupon.addEventListener('keyup' , function (event){
+    text = event.target.value;
+    const applyButton = document.getElementById('btn-apply');
+    if (text == "NEW15" || text == "Couple 20") {
+        applyButton.removeAttribute("disabled");
+    } else {
+        applyButton.setAttribute("disabled", true);
+    }
+  })
+
+  
---
+++ scripts/seating.js
@@ -0,0 +1,56 @@
+function setInnerText(id, value) {
+  document.getElementById(id).innerText = value;
+}
+
+const allSeatButton = document.getElementsByClassName("btn-seat-select");
+let seatCount = 0;
+let seatCountDecrease = 28;
+
+for (const button of allSeatButton) {
+  button.addEventListener("click", function (event) {
+    const seatNum = event.target.innerText;
+    console.log(seatNum);
+    seatCount++;
+    setInnerText("seat-count", seatCount);
+    seatCountDecrease--;
+    setInnerText("seat-count-decrease", seatCountDecrease);
+    if (seatCount >= 1) {
+      button.setAttribute("disabled", true);
+    } else {
+      button.removeAttribute("disabled", true);
+    }
+
+    const selectedSeatContainer = document.getElementById(
+      "selected-seat-container"
+    );
+
+    const div = document.createElement("div");
+    const p = document.createElement("p");
+    p.innerText = seatNum;
+    const p2 = document.createElement("p");
+    p2.innerText = "Economy";
+    const p3 = document.createElement("p");
+    p3.innerText = "550";
+
+    div.appendChild(p)
+    div.appendChild(p2)
+    div.appendChild(p3)
+    div.classList.add('flex','justify-between','border-b-2','border-[#03071233]','py-4','font-normal','text-[#03071299]','text-base')
+    selectedSeatContainer.appendChild(div);
+
+    const totalPriceStr = document.getElementById("total-price").innerText;
+    const totalPrice = parseInt(totalPriceStr);
+    const grandTotalStr = document.getElementById("grand-total").innerText;
+    const grandTotal = parseInt(grandTotalStr);
+
+    setInnerText('total-price', totalPrice+550);
+
+   const finalPrice = document.getElementById("grand-total").innerText = grandTotal+550;
+
+   document.getElementById('btn-apply').addEventListener('click',function (){
+    document.getElementById("grand-total").innerText = finalPrice-(totalPrice*0.15);
+    const couponArea = document.getElementById('coupon-section');
+    couponArea.classList.add('hidden');
+ }) 
+  });
+}
